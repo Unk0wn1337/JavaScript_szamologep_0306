4a8e8267079937fd6aea3ded412ed275844a652b
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2024-03-09 10:35:36 +0100

    neha az eval nem mukodik rendesen, de elvegzi a muveletek

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ function szamok(){
     const KIJELZO = document.querySelector(".kijelzo");
     const SZAMOK_ELEM = document.querySelector(".szamok");
     let kiiras = ``;
-    const SZAMOK = []
+    const SZAMOK = [];
+    const MUVELEK = [];
 
     //GOMBOK DEKLARÁLÁSA
     const TORLES_BTN = document.getElementById("torles");
     const OSSZEADAS_BTN = document.getElementById("osszeadas");
+    const KIVONAS_BTN = document.getElementById("kivonas");
+    const SZORZAS_BTN = document.getElementById("szorzas");
+    const OSZTAS_BTN = document.getElementById("osztas");
+    const LEBEGO_PONTOS_BTN = document.getElementById(".");
+    const EGYENLO_BTN = document.getElementById("egyenlo");
 
 
     // szám gombok kiirasa
@@ -29,8 +35,9 @@ function szamok(){
     // szamok gombok gombra való esemény megjelenitese
     const SZAMElem=document.querySelectorAll(".szam")
     for (let index = 0; index < SZAMOK.length; index++) {
-        SZAMElem[index].addEventListener("click", function() {
+        SZAMElem[index].addEventListener("click", function(event) {
             KIJELZO.innerHTML += SZAMOK[index];
+            console.log("Kattintott elem", event.target)
         });
     }
 
@@ -41,7 +48,37 @@ function szamok(){
     });
     
     
-        
+    // osszeadas
+    OSSZEADAS_BTN.addEventListener("click",function() {
+        KIJELZO.innerHTML += "+"; 
+    });
+    KIVONAS_BTN.addEventListener("click",function() {
+        KIJELZO.innerHTML += "-"; 
+    });
+    SZORZAS_BTN.addEventListener("click",function() {
+        KIJELZO.innerHTML += "*"; 
+    });
+    OSZTAS_BTN.addEventListener("click",function() {
+        KIJELZO.innerHTML += "/"; 
+    });
+
+    LEBEGO_PONTOS_BTN.addEventListener("click",function() {
+        KIJELZO.innerHTML += "."; 
+    });
+    EGYENLO_BTN.addEventListener("click",function() {
+        let szamokMuvelet = "";
+        for (let index = 0; index < KIJELZO.innerHTML.length; index++) {
+            const karakter = KIJELZO.innerHTML[index];
+            if(karakter !== "."){
+                szamokMuvelet += karakter;
+            } else {
+                szamokMuvelet += "."
+            }
+        }
+        let eredmeny = eval(szamokMuvelet);
+        KIJELZO.innerHTML = eredmeny;
+    });
+    
     
 
     
